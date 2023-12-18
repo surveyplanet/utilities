798f3f12f614e3afbb55459e9692d14e8d3f7c71
serferdinand2 <ognjen@surveyplanet.com>
2023-12-18 19:26:08 +0100

    fixed dateToString and stringToDate utilitiy functions

--- src/date_to_string.ts
+++ src/date_to_string.ts
@@ -26,16 +26,6 @@ const dateToString = (
 	} else {
 		return dateVal.toISOString().split('T')[0]; // date only
 	}
-
-	// if (isNaN(dateVal.getTime())) {
-	// 	return '';
-	// } else if (type === 'datetime-local') {
-	// 	return dateVal.toISOString().split('.')[0]; // datetime-local
-	// } else if (type === 'time') {
-	// 	return dateVal.toISOString().split('T')[1].split('.')[0]; // time only
-	// } else {
-	// 	return dateVal.toISOString().split('T')[0]; // date only
-	// }
 };
 
 export default dateToString;
--- src/tests/string_to_date.test.ts
+++ src/tests/string_to_date.test.ts
@@ -2,22 +2,27 @@ import { test, expect, describe } from 'vitest';
 import stringToDate from '../string_to_date';
 
 describe('stringToDate', () => {
-	test.only('should return undefined if empty string', () => {
+	test('should return undefined if empty string', () => {
 		expect(stringToDate('time', '')).toBe(undefined);
 	});
-	test.only('should return undefined if invalid time', () => {
+	test('should return undefined if invalid time', () => {
 		expect(stringToDate('time', '11:35')?.toISOString()).toBe(
-			'1970-01-01T11:35:00.000Z'
+			'1899-12-31T11:35:00.000Z'
 		);
 	});
-	test.only('should return undefined if invalid date', () => {
+	test('should return undefined if invalid date', () => {
 		expect(stringToDate('date', '1977-04-29')?.toISOString()).toBe(
 			'1977-04-29T00:00:00.000Z'
 		);
 	});
 	test('should return undefined if invalid datetime-local', () => {
 		expect(
-			stringToDate('datetime-local', '1977-03-29T06:00:00')?.toISOString()
-		).toBe('1977-03-29T06:00:00.000Z');
+			stringToDate('datetime-local', '2023-05-29T06:00:00')?.toISOString()
+		).toBe('2023-05-29T06:00:00.000Z');
+	});
+	test('should return correct date for leap year', () => {
+		expect(stringToDate('date', '2020-02-29')?.toISOString()).toBe(
+			'2020-02-29T00:00:00.000Z'
+		);
 	});
 });
