eecb7cee2ca8729decf6060307193b8dedc8f0ac
Jonah Werre <jonahwerre@gmail.com>
2023-12-22 09:38:33 -0700

    refactor date functions

--- src/date_to_string.ts
+++ src/date_to_string.ts
@@ -1,33 +1,33 @@
-import { ISODate } from '@surveyplanet/types';
+import { DateTimeValue } from '@surveyplanet/types';
+import InputType from './types/date_time_input_type';
 
 /**
- * Description....
- *
  * @name dateToString
  * @returns {ISODate}
  */
-const dateToString = (type: InputType): ISODate => {
-	if (!response?.length) {
-		return '';
+const dateToString = (
+	type: InputType,
+	response: DateTimeValue[]
+): string | undefined => {
+	if (!response.length) {
+		return;
 	}
 
 	const dateVal = new Date(response[0]);
 
 	if (isNaN(dateVal.getTime())) {
-		return '';
+		return;
 	}
 
-	if (date && time) {
-		return dateVal.toISOString().split('.')[0]; // datetime-local
-	}
-
-	if (time) {
+	if (type === 'time') {
 		return dateVal.toISOString().split('T')[1].split('.')[0]; // time only
 	}
 
-	if (date) {
+	if (type === 'date') {
 		return dateVal.toISOString().split('T')[0]; // date only
 	}
 
-	return '';
+	return dateVal.toISOString().split('.')[0]; // datetime-local
 };
+
+export default dateToString;
--- src/string_to_date.ts
+++ src/string_to_date.ts
@@ -1,53 +1,50 @@
+import {
+	type default as DateTimeInputType,
+	isDateTimeInputType,
+} from './types/date_time_input_type';
+
 /**
- * Description.....
- *
- * time: 11:35
- * date: 1977-04-29
- * datetime-local: 1977-03-29T06:00:00
  * @name stringToDate
  * @returns {Date}
  */
 
-const stringToDate = (type: InputType, isoStr: string): Date | undefined => {
-	console.log('stringToDate', isoStr);
+const stringToDate = (
+	type: DateTimeInputType,
+	isoStr: string
+): Date | undefined => {
+	if (!isDateTimeInputType(type)) {
+		throw new Error(`Invalid type: ${type as string}`);
+	}
 
-	if (!isoStr?.length) {
+	if (!isoStr.length) {
 		return;
 	}
 
 	isoStr = isoStr.trim(); // clean up whitespace
 
-	let result: Date;
-
 	// Time format only e.g.: 11:35
 	if (type === 'time') {
 		const [hr, min] = isoStr.split(':').map(Number);
 
-		if (isNaN(hr) || isNaN(min)) {
-			return;
-		}
-		// BUG: this will be in the user's timezone instead of UTC, need to
-		// use Intl API: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl
-		result = new Date(0, 0, 0);
-		result.setUTCHours(hr);
-		result.setUTCMinutes(min);
-	} else if (type === 'date') {
-	} else if (type === 'datetime-local') {
-		// This should work for both date and datetime formats e.g.: 1977-04-29 or 1977-03-29T06:00:00
-		if (!time) {
-			result = new Date(isoStr); // BUG: same bug as above.
-		} else {
-			const [d, t] = isoStr.split('T');
-			const [hr, min] = t.split(':').map(Number);
-			result = new Date(d);
-			result.setUTCHours(hr);
-			result.setUTCMinutes(min);
-		}
+		// if (isNaN(hr) || isNaN(min)) {
+		// 	return;
+		// }
+
+		return new Date(Date.UTC(0, 0, 0, hr, min));
 	}
 
-	if (isNaN(result.getTime())) {
-		return;
+	if (type === 'date') {
+		const [year, month, day] = isoStr.split('-').map(Number);
+		return new Date(Date.UTC(year, month - 1, day));
 	}
 
-	return result;
+	// if (type === 'datetime-local') {
+	const [d, t] = isoStr.split('T');
+	const [year, month, day] = d.split('-').map(Number);
+	const [hr, min] = t.split(':').map(Number);
+
+	return new Date(Date.UTC(year, month - 1, day, hr, min));
+	// }
 };
+
+export default stringToDate;
