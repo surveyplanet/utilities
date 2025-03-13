444b55366a45e8dbad1d102a6a1e0c064699104f
Jonah Werre <jonahwerre@gmail.com>
2025-03-13 12:35:37 -0600

    fix: refactor locals and session

--- src/locals.ts
+++ src/locals.ts
@@ -1,75 +1,89 @@
 const TEST_NAME = '__local_storage_test__';
 const TEST_VAL = 'yes';
 
+/**
+ * Check if localStorage is available
+ *
+ * @function available
+ * @returns {boolean}
+ */
+function available(): boolean {
+	if (typeof localStorage === 'undefined') {
+		return false;
+	}
+
+	try {
+		localStorage.setItem(TEST_NAME, TEST_VAL);
+		if (localStorage.getItem(TEST_NAME) === TEST_VAL) {
+			localStorage.removeItem(TEST_NAME);
+			return true;
+		} else {
+			return false;
+		}
+	} catch (e) {
+		return false;
+	}
+}
+
+/**
+ * Creates a local storage value. This is a wrapper around localStorage.setItem() where all values are stringified.
+ *
+ * @method set
+ * @param {string} name The name of the local storage value
+ * @param {T} value The value of the local object
+ */
+function set<T>(name: string, value: T) {
+	if (!available()) {
+		return;
+	}
+	const val = JSON.stringify(value);
+	localStorage.setItem(name, val);
+}
+
+/**
+ * Retrieves a value from local storage.
+ *
+ * @method get
+ * @param {string} name The name of the local storage value being retrieved
+ * @param {D} defaultVal The default value to return if the value is not found
+ * @return The retrieved value or default value
+ */
+function get<T, D extends T | undefined = undefined>(
+	name: string,
+	defaultVal?: D
+): D extends undefined ? T | null : T {
+	if (!available()) {
+		return (defaultVal ?? null) as D extends undefined ? T | null : T;
+	}
+	const value = localStorage.getItem(name);
+
+	if (value === null) {
+		return (defaultVal ?? null) as D extends undefined ? T | null : T;
+	}
+
+	try {
+		return JSON.parse(value) as T;
+	} catch (error) {
+		return (defaultVal ?? null) as D extends undefined ? T | null : T;
+	}
+}
+
+/**
+ * Removes a local object.
+ *
+ * @method remove
+ * @param {string} name The name of the local object being removed
+ */
+function remove(name: string) {
+	if (!available()) {
+		return;
+	}
+	localStorage.removeItem(name);
+}
+
 export const locals = {
-	/**
-	 * Check if localStorage is available
-	 *
-	 * @function available
-	 * @returns {boolean}
-	 */
-	available: (): boolean => {
-		if (typeof localStorage === 'undefined') {
-			return false;
-		}
-
-		try {
-			localStorage.setItem(TEST_NAME, TEST_VAL);
-			if (localStorage.getItem(TEST_NAME) === TEST_VAL) {
-				localStorage.removeItem(TEST_NAME);
-				return true;
-			} else {
-				return false;
-			}
-		} catch (e) {
-			return false;
-		}
-	},
-
-	/**
-	 * Creates a local storage value. This is a wrapper around localStorage.setItem() where all values are stringified.
-	 *
-	 * @method set
-	 * @param {string} name The name of the local storage value
-	 * @param {T} value The value of the local object
-	 */
-	set: <T>(name: string, value: T) => {
-		const val = JSON.stringify(value);
-		localStorage.setItem(name, val);
-	},
-
-	/**
-	 * Retrieves a value from local storage.
-	 *
-	 * @method get
-	 * @param {string} name The name of the local storage value being retrieved
-	 * @param {D} defaultVal The default value to return if the value is not found
-	 * @return The retrieved value or default value
-	 */
-	get: <T, D extends T | undefined = undefined>(
-		name: string,
-		defaultVal?: D
-	): D extends undefined ? T | null : T => {
-		const value = localStorage.getItem(name);
-
-		if (value === null) {
-			return (defaultVal ?? null) as D extends undefined ? T | null : T;
-		}
-
-		try {
-			return JSON.parse(value) as T;
-		} catch (error) {
-			return (defaultVal ?? null) as D extends undefined ? T | null : T;
-		}
-	},
-
-	/**
-	 * Removes a local object.
-	 *
-	 * @method remove
-	 * @param {string} name The name of the local object being removed
-	 */
-	remove: (name: string) => {
-		localStorage.removeItem(name);
-	},
+	available,
+	set,
+	get,
+	remove,
 } as const;
--- src/session.ts
+++ src/session.ts
@@ -1,75 +1,91 @@
 const TEST_NAME = '__session_storage_test__';
 const TEST_VAL = 'yes';
 
+/**
+ * Check if sessionStorage is available
+ *
+ * @function available
+ * @returns {boolean}
+ */
+function available(): boolean {
+	if (typeof sessionStorage === 'undefined') {
+		return false;
+	}
+
+	try {
+		sessionStorage.setItem(TEST_NAME, TEST_VAL);
+		if (sessionStorage.getItem(TEST_NAME) === TEST_VAL) {
+			sessionStorage.removeItem(TEST_NAME);
+			return true;
+		} else {
+			return false;
+		}
+	} catch (e) {
+		return false;
+	}
+}
+
+/**
+ * Creates a session value. This is a wrapper around sessionStorage.setItem() where all values are stringified.
+ *
+ * @method set
+ * @param {string} name The name of the session object
+ * @param {T} value The value of the session object
+ */
+function set<T>(name: string, value: T) {
+	if (!available()) {
+		return;
+	}
+
+	const val = JSON.stringify(value);
+	sessionStorage.setItem(name, val);
+}
+
+/**
+ * Retrieves a value from session storage.
+ *
+ * @method get
+ * @param {string} name The name of the session storage value being retrieved
+ * @param {D} defaultVal The default value to return if the value is not found
+ * @return The retrieved value or default value
+ */
+function get<T, D extends T | undefined = undefined>(
+	name: string,
+	defaultVal?: D
+): D extends undefined ? T | null : T {
+	if (!available()) {
+		return (defaultVal ?? null) as D extends undefined ? T | null : T;
+	}
+
+	const value = sessionStorage.getItem(name);
+
+	if (value === null) {
+		return (defaultVal ?? null) as D extends undefined ? T | null : T;
+	}
+
+	try {
+		return JSON.parse(value) as T;
+	} catch (error) {
+		return (defaultVal ?? null) as D extends undefined ? T | null : T;
+	}
+}
+
+/**
+ * Removes a session object.
+ *
+ * @method remove
+ * @param {string} name The name of the session object being removed
+ */
+function remove(name: string) {
+	if (!available()) {
+		return;
+	}
+	sessionStorage.removeItem(name);
+}
+
 export const session = {
-	/**
-	 * Check if sessionStorage is available
-	 *
-	 * @function available
-	 * @returns {boolean}
-	 */
-	available: (): boolean => {
-		if (typeof sessionStorage === 'undefined') {
-			return false;
-		}
-
-		try {
-			sessionStorage.setItem(TEST_NAME, TEST_VAL);
-			if (sessionStorage.getItem(TEST_NAME) === TEST_VAL) {
-				sessionStorage.removeItem(TEST_NAME);
-				return true;
-			} else {
-				return false;
-			}
-		} catch (e) {
-			return false;
-		}
-	},
-
-	/**
-	 * Creates a session value. This is a wrapper around sessionStorage.setItem() where all values are stringified.
-	 *
-	 * @method set
-	 * @param {string} name The name of the session object
-	 * @param {T} value The value of the session object
-	 */
-	set: <T>(name: string, value: T) => {
-		const val = JSON.stringify(value);
-		sessionStorage.setItem(name, val);
-	},
-
-	/**
-	 * Retrieves a value from session storage.
-	 *
-	 * @method get
-	 * @param {string} name The name of the session storage value being retrieved
-	 * @param {D} defaultVal The default value to return if the value is not found
-	 * @return The retrieved value or default value
-	 */
-	get: <T, D extends T | undefined = undefined>(
-		name: string,
-		defaultVal?: D
-	): D extends undefined ? T | null : T => {
-		const value = sessionStorage.getItem(name);
-
-		if (value === null) {
-			return (defaultVal ?? null) as D extends undefined ? T | null : T;
-		}
-
-		try {
-			return JSON.parse(value) as T;
-		} catch (error) {
-			return (defaultVal ?? null) as D extends undefined ? T | null : T;
-		}
-	},
-
-	/**
-	 * Removes a session object.
-	 *
-	 * @method remove
-	 * @param {string} name The name of the session object being removed
-	 */
-	remove: (name: string) => {
-		sessionStorage.removeItem(name);
-	},
+	available,
+	set,
+	get,
+	remove,
 } as const;
